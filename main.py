6195cbdfa5d7efa071b3a9ee85f59fd9c15d2a75
William Edwards <spaceiskool1998@gmail.com>
2023-02-08 03:26:47 -0500

    Added a way to find the shortest path, and Layovers

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
+
 from dijsktra import Graph
     
-def run_sample():
-  edges = [
-    ('X', 'A', 7),
-    ('X', 'B', 2),
-    ('X', 'C', 3),
-    ('X', 'E', 4),
-    ('A', 'B', 3),
-    ('A', 'D', 4),
-    ('B', 'D', 4),
-    ('B', 'H', 5),
-    ('C', 'L', 2),
-    ('D', 'F', 1),
-    ('F', 'H', 3),
-    ('G', 'H', 2),
-    ('G', 'Y', 2),
-    ('I', 'J', 6),
-    ('I', 'K', 4),
-    ('I', 'L', 4),
-    ('J', 'L', 1),
-    ('K', 'Y', 5),
+def run_gas_graph():
+  edges =[
+    ("Vn", "LV", 327),
+    ("Vn", "Bo", 561),
+    ("Vn", "NY", 603),
+    ("Mi", "NY", 237),
+    ("Ch", "NY", 226),
+    ("Ch", "LV", 254),
+    ("NY", "FL", 299),
+    ("NY", "LV", 617),
+    ("NY", "Bo", 133),
+    ("FL", "LV", 99),
+    ("FL", "LA", 676),
+    ("LA", "SL", 222),
+    ("LV", "SL", 69),
+    ("FL", "Bo", 465),
+    ("LA", "LV", 169)
+
   ]
-  
   graph = Graph(edges)
-  start = 'Y'
-  stop = 'X'
-  path = graph.dijsktra_shortest_path(start, stop)
-  print (path)
+  start = 'SL'
+  stop = 'Mi'
   
+  path = graph.dijsktra_shortest_path(start, stop)
+  
+  (S, *L, F) = path
+  
+  layover = 0
+
+  while layover < len(L):
+    
+    layover = layover + 1 
+ 
+  print (str("There is"), int(layover), str("Layovers they are"), L, str("."))
+  print(str("This is your flight path:"), path)
+
+
+
 
 def main():
-  run_sample()
+  
+  run_gas_graph()
 
 if __name__ == "__main__":
   main()
